5298e0877396f2a016013dd231a5bb1e1db0cf30
Wendi Zhou <wendi-zhou@berkeley.edu>
2020-02-23 18:23:09 -0800

    infinite scrolling and player jump

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpSpeed;
+    private Rigidbody2D playerRB;
+
+    private bool isGrounded;
+
+
+    [SerializeField]
+    Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+
+    private void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //RaycastHit2D groundRayCast = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        //if (groundRayCast)
+        //{
+        //    Debug.Log("isGrounded");
+        //    isGrounded = true;
+        //} else
+        //{
+        //    isGrounded = false;
+        //}
+
+        if ((Input.GetKey(KeyCode.Space) || Input.GetKey("up")) && isGrounded)
+        {
+            Jump();
+
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+    }
+
+
+    void Jump()
+    {
+        Vector2 jumpVector = new Vector2(playerRB.velocity.x, jumpSpeed);
+        playerRB.velocity = jumpVector;
+    }
+}
